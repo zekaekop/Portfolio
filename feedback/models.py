193d549fbccef256b07c0993f5c26f3cb12dee43
zekaeko.p <zekaeko.p@gmail.com>
2026-03-04 15:01:53 +0300

    Added an feedback app

---
+++ feedback/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Feedback(models.Model):
+    ip_addr = models.GenericIPAddressField
+    
+    name = models.CharField(max_length=200,verbose_name="Name")
+    desc = models.CharField(max_length=400, verbose_name="Description")
+
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    viewed = models.BooleanField(default=False)
+    time_viewed = models.TimeField(auto_now=True)
---
+++ feedback/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, reverse
+from . import views
+
+app_name = "feedback"
+
+urlpatterns = [
+    path('/report', views.report , name="report"),
+]
