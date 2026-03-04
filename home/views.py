1132242217964940db2625a7d8bf25de08c51971
zekaeko.p <zekaeko.p@gmail.com>
2026-03-04 10:55:35 +0300

    added the list of tag lagnuages and frameworks and also added admin_panel link (Not made yet)

--- home/views.py
+++ home/views.py
@@ -3,4 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def home_page(request):
-    return render(request, "home/home.html", {"world":"World!!"})
+
+    frameworks = ("Django", "Nginx", "NodeJS", ) # ETC.
+    tags = ("CSS","HTML","JS","C","C++","Python","PHP",)
+
+    context = {
+        "world":"World!!",
+        "tags":  tags,
+        "frameworks": frameworks,
+    }
+    
+    return render(request, "home/home.html", context)
