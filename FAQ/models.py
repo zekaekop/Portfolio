9f82d9d0f2760e4b375f86206550dcd3d0a1764f
zekaeko.p <zekaeko.p@gmail.com>
2026-03-06 12:25:33 +0300

    removed onetoone field on FAQ to allow multiple

--- FAQ/models.py
+++ FAQ/models.py
@@ -3,14 +3,14 @@ from account.models import Anon
 
 # Create your models here.
 class FAQuestions(models.Model):
-    anon = models.OneToOneField(Anon, on_delete=models.CASCADE)
+    anon = models.ForeignKey(Anon, on_delete=models.CASCADE)
 
     asked_question = models.TextField(null=False, blank=False)
     answer = models.TextField(null=False, blank=False)
     created_date = models.DateTimeField(auto_now_add=True)
 
 class FAQuestionsSubmissions(models.Model):
-    anon = models.OneToOneField(Anon, on_delete=models.CASCADE)
+    anon = models.ForeignKey(Anon, on_delete=models.CASCADE)
 
     question = models.TextField(null=False, blank=False)
     desc = models.CharField(max_length=600 ,null=False, blank=False)
