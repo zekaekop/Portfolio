2c27600a286c0b1b7b1ac39e2933ff131cac5e67
zekaeko.p <zekaeko.p@gmail.com>
2026-03-06 12:19:25 +0300

    fixes feedback form not being able to submit more than one (apperantly i accidently did onetoonefield instead of foreignkey since onetoone does not accept more than one)

--- feedback/models.py
+++ feedback/models.py
@@ -4,10 +4,10 @@ from account.models import Anon
 # Create your models here.
 class Feedback(models.Model):
 
-    anon = models.OneToOneField(Anon, on_delete=models.CASCADE) 
+    anon = models.ForeignKey(Anon, on_delete=models.CASCADE)
     
-    title = models.CharField(max_length=200,verbose_name="Title")
-    desc = models.CharField(max_length=400, verbose_name="Description")
+    title = models.CharField(max_length=200,verbose_name="Title", blank=False, null=False)
+    desc = models.CharField(max_length=400, verbose_name="Description",  blank=False, null=False)
 
     date_created = models.DateTimeField(auto_now_add=True)
 
