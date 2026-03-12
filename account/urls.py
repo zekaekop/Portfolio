ba14e6ed684952b41348d42f4257b6f9c0810896
zekaeko.p <zekaeko.p@gmail.com>
2026-03-12 11:42:06 +0300

    added register url to account urls

--- account/urls.py
+++ account/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'account'
 
 urlpatterns = [
+    path('register/', views.register , name="register"),
     path('login/', views.login , name="login"),
     path('logout/', views.logout , name="logout"),
 ]
