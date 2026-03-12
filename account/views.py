00cf4e29275a2c24ccc5301c62438529d68d5d0e
zekaeko.p <zekaeko.p@gmail.com>
2026-03-12 11:41:26 +0300

    added basic non working login logout register views

--- account/views.py
+++ account/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
 # Create your views here.
+
+class Account:
+
+    def register(request):
+        login_status = False
+        if request.POST.method == "POST":
+            
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+            repeat_password = request.POST.get("repeat_password")
+
+            return render(request, "/", {"login_status": status})
+        return render(request, "/", {"login_status": status})
+
+
+    def login(request):
+        if request.POST.method == "POST":
+            
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+
+    def logout(request):
+        logout(request)
+        redirect("account/login")
