3e23ae39ac591bbe1a72a604a389ae0a5dec1a9e
zekaeko.p <zekaeko.p@gmail.com>
2026-03-13 16:03:41 +0300

    added post model, i will make a better The Django Project post system over again.

--- imageboard/models.py
+++ imageboard/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from account.models import Anon
 # Create your models here.
+
+class Post:
+    original_poster = models.ForeignKey(Anon, on_delete=models.CASCADE)
+    title = models.CharField(null=False, blank=False)
+    desc = models.CharField(null=True, blank=True)
+    file = models.FileField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    created_date = models.DateTimeField(auto_created=True)
